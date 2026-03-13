998de96312a9a08f6a333c9c401c855ca813ed7c
Alvaro Perez <alvaro.perez1@alu.uhu.es>
2026-03-13 14:06:54 +0100

    first commit

---
+++ src/main/java/com/example/mgsc/MgscApplication.java
@@ -0,0 +1,13 @@
+package com.example.mgsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgscApplication.class, args);
+	}
+
+}
