e92c10258e839586ada54f92dd2074372f610c52
AlvPru <alvaroperezromero01@gmail.com>
2026-03-29 18:56:04 +0200

    Remove unused application and domain classes related to client, request, and technician management

--- src/main/java/com/example/mgsc/dominio/EstadoSolicitud.java
+++
@@ -1,7 +0,0 @@
-package com.example.mgsc.dominio;
-
-public enum EstadoSolicitud {
-    ABIERTA,
-    EN_PROCESO,
-    CERRADA
-}
--- src/main/java/com/example/mgsc/dominio/TipoCliente.java
+++
@@ -1,6 +0,0 @@
-package com.example.mgsc.dominio;
-
-public enum TipoCliente {
-    STANDARD,
-    PREMIUM
-}
