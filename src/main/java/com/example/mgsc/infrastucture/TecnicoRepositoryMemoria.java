31abe25d61c94218634192d621e5bd7b8d3c199e
FernanRLA <fernandillorla@gmail.com>
2026-04-02 19:10:43 +0200

    TDD Verde: Lógica implementada para pasar los tests

--- src/main/java/com/example/mgsc/infrastucture/TecnicoRepositoryMemoria.java
+++ src/main/java/com/example/mgsc/infrastucture/TecnicoRepositoryMemoria.java
@@ -2,21 +2,16 @@ package com.example.mgsc.infrastucture;
 
 import com.example.mgsc.dominio.Tecnico;
 import com.example.mgsc.service.TecnicoRepositoryPort;
+import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class TecnicoRepositoryMemoria implements TecnicoRepositoryPort {
-    private static TecnicoRepositoryMemoria instance;
     private final List<Tecnico> tecnicos = new ArrayList<>();
 
-    private TecnicoRepositoryMemoria() {}
-
-    public static TecnicoRepositoryMemoria getInstance() {
-        if (instance == null) {
-            instance = new TecnicoRepositoryMemoria();
-        }
-        return instance;
-    }
+    // Constructor público para que Spring Boot pueda usarlo
+    public TecnicoRepositoryMemoria() {}
 
     @Override
     public void guardar(Tecnico tecnico) {
