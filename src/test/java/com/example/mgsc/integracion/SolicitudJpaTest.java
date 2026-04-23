dab70523d191057960d9d31495baa4c392e7556c
AlvPru <alvaroperezromero01@gmail.com>
2026-04-23 17:47:32 +0200

    Aumento de la cobertura, test para la entidad de tecnico

--- src/test/java/com/example/mgsc/integracion/SolicitudJpaTest.java
+++ src/test/java/com/example/mgsc/integracion/SolicitudJpaTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import com.example.mgsc.dominio.Cliente;
 import com.example.mgsc.dominio.Solicitud;
+import com.example.mgsc.dominio.Tecnico;
 import com.example.mgsc.dominio.TipoCliente;
 import com.example.mgsc.dominio.entidades.SolicitudEntity;
 
@@ -32,5 +33,18 @@ class SolicitudJpaTest {
 
         Optional<SolicitudEntity> solicitudRecuperada = repository.findById(entity.getId());
         assertEquals("Esto es de prueba", solicitudRecuperada.get().toDomain().getDescripcion());
+    }
+
+    @Test
+    void guardarYRecuperarSolicitudConTecnico() {
+        Cliente cliente = new Cliente(666, "Juan Pérez", "juan.perez@example.com", TipoCliente.PREMIUM);
+        Solicitud solicitud = new Solicitud("Esto es de prueba", cliente);
+        Tecnico tecnico = new Tecnico(1, "Ana", "Electricidad", true);
+        solicitud.setTecnico(tecnico);
+        SolicitudEntity entity = SolicitudEntity.fromDomain(solicitud);
+        repository.save(entity);
+
+        Optional<SolicitudEntity> solicitudRecuperada = repository.findById(entity.getId());
+        assertEquals("Ana", solicitudRecuperada.get().toDomain().getTecnico().getNombre());
     }
 }
