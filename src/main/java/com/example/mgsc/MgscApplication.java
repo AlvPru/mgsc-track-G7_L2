89d15cf1b4e09b5ce1667a12a56431a75d3f7bd8
FernanRLA <fernandillorla@gmail.com>
2026-05-09 18:44:21 +0200

    feat: Implementados DTOs, Controlador REST y Swagger para Tecnico (Sesion 10)

--- src/main/java/com/example/mgsc/MgscApplication.java
+++ src/main/java/com/example/mgsc/MgscApplication.java
@@ -1,13 +1,11 @@
 package com.example.mgsc;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class MgscApplication {
-
-	public static void main(String[] args) {
-		//Clase main pendiente de implementación, se puede usar para pruebas rápidas o para iniciar la aplicación si se decide agregar una interfaz de usuario o API REST en el futuro.
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MgscApplication.class, args);
+    }
 }
